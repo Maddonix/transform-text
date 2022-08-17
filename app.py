f4b0182d6899a73beabcf07dd3b94d0ae8e3cd26
Thomas J. Lux <tlux14@gmail.com>
2022-08-17 13:34:04 +0200

    added text parser

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-# st.title("Transform your texts")
+st.title("Transform your texts")
+
+font_size = "12px"
+p_normal = f"<p style='font-size:{font_size};'>"
+p_bold = f"<p style='font-size:{font_size}; font-weight: bold'>"
+p_bold_underlined = f"<p style='font-size:{font_size}; font-weight: bold;text-decoration:underline'>"
+INPUT_AREA_HEIGHT = 200
 
 
-st.write("stuff")
+raw = st.text_area("Enter Text to remove spaces:", height = INPUT_AREA_HEIGHT)
+if raw:
+    st.write(raw)
